1ec394e4fce159fe4e8aa5e30602cc343683dc2d
Brandon Araujo <bac2795@gmail.com>
2025-11-30 20:13:04 -0500

    Changes

--- envs/prd/backend.tf
+++ envs/prd/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cicd-terraform-app-araujobm" # Reemplazar por araujobm
-    storage_account_name = "tfstatearaujobm"                # Reemplazar por araujobm
+    resource_group_name  = "rg-cicd-terraform-app-araujobmw" # Reemplazar por apellido
+    storage_account_name = "tfstatearaujobmw"                # Reemplazar por apellido
     container_name       = "tfstate"
     key                  = "prd/terraform.tfstate"
   }
--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 locals {
-  idapp = "araujobm" # araujobm
+  idapp = "araujobmw" # apellido
 }
 
 terraform {
